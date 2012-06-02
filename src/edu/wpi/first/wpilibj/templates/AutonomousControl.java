16ac3ac2d7e96379cfd6ce0a4363c49ca4d1bb35
Jaden McConkey <jadenmcconkey@gmail.com>
2012-06-02 01:48:19 -0600

    Updating repository with new could still experimental have not yet had the chance to do proof of concept testing Still trying to get joel to use git instead of dropbox. Joel must learn to PLAN and inform other people of ideas and actions being taken also he needs to learn when / how to comment things properly (when being all the time.)

--- src/edu/wpi/first/wpilibj/templates/AutonomousControl.java
+++
@@ -1,9 +0,0 @@
-package edu.wpi.first.wpilibj.templates;
-public class AutonomousControl extends Robot4334{
-    public void autonomousMode() {
-        
-    }
-    public void testing() {
-        
-    }
-}
